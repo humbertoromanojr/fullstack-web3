def57e4b7016af6bef8e430f32721f3ce5bfa412
humbertojr <astronomi@gmail.com>
2025-06-03 15:56:11 -0300

    First commit

---
+++ dapp/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div>
+        <h1>Web3 Todo List</h1>
+        <h2>Gerencie sua tarefas com web3</h2>
+      </div>
+    </div>
+  );
+}
