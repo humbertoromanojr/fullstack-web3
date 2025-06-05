e0a2d632ef1217e5d6c58baaf8e1090d40f83eaf
Humberto Junior <astronomi@gmail.com>
2025-06-04 22:25:10 -0300

    Finished TaskCard common component

--- dapp/src/components/common/taskCard.tsx
+++ dapp/src/components/common/taskCard.tsx
@@ -1,12 +1,50 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 
-export function TaskCard() {
+import { Badge } from "../ui/badge";
+
+interface TaskCardProps {
+  title: string;
+  description: string;
+  dateCreated: string;
+  expirationDate: string;
+  stake: number;
+}
+
+export function TaskCard({
+  title,
+  description,
+  dateCreated,
+  expirationDate,
+  stake,
+}: TaskCardProps) {
   return (
-    <Card className="flex flex-col gap-2">
-      <CardHeader>
-        <h2>Task Card</h2>
+    <Card>
+      <CardHeader className="flex">
+        <div className="flex flex-col">
+          <h1 className="text-lg font-bold">{title}</h1>
+        </div>
+        <Badge variant="default">Pending</Badge>
       </CardHeader>
-      <CardContent>3</CardContent>
+      <CardContent>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </CardContent>
+      <CardFooter className="flex justify-between gap-2">
+        <div className="flex gap-2">
+          <p className="text-sm text-muted-foreground">
+            <span className="font-bold">Date created:</span>
+            {dateCreated}
+          </p>
+          <p className="text-sm text-muted-foreground">
+            <span className="font-bold">Expiration date:</span> {expirationDate}
+          </p>
+        </div>
+        <span>{stake} wei</span>
+      </CardFooter>
     </Card>
   );
 }
