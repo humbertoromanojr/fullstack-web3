15c3d25595ea7b538d5081f1348088562061d263
Humberto Junior <astronomi@gmail.com>
2025-06-06 11:01:00 -0300

    Smart Contract example

---
+++ todoStake/script/TaskManager.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {TaskManager} from "../src/TaskManager.sol";
+
+contract TaskManagerScript is Script {
+    TaskManager public taskManager;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        taskManager = new TaskManager();
+
+        vm.stopBroadcast();
+    }
+}
