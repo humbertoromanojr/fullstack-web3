cfc502c7e45ba958a06012242cf3e70687b84c1e
Humberto Junior <astronomi@gmail.com>
2025-07-16 21:00:59 -0300

    Complete function run

--- todoStake/script/TaskManager.s.sol
+++ todoStake/script/TaskManager.s.sol
@@ -10,9 +10,10 @@ contract TaskManagerScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
 
         taskManager = new TaskManager();
+        console.log('TaskManager deployed to: ', address(taskManager));
 
         vm.stopBroadcast();
     }
